92da83fdf95b5fc015f5b9f8b90ad7c05afb95b3
JohnNguyen29 <johnnguyen290107@gmail.com>
2024-08-15 23:13:49 +1200

    Landing Page v2;
    I have chose to split the landing page into 3 parts. The navigation bar on the left side is in v1. v2 is the top frame where the welcome message will be and the progress bars and pie chart of the users credits achieved will be displayed.

--- flandingpg.py
+++ flandingpg.py
@@ -1,34 +1,44 @@
 import tkinter as tk
-from tkinter import *
-from PIL import ImageTk, Image
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 root = tk.Tk()
 root.title("Landing Page")
 root.geometry("1440x900")
 
-def overlay_image():
-    # Image
-    my_img = Image.open("/Users/nguyennguyen/Desktop/templogo.png")
-    resized = my_img.resize((200, 100), Image.LANCZOS)
+# Top frame
+top_frame = tk.Frame(root, bg="lightblue", width=400, height=450)
+top_frame.pack(side="top", fill="x")
 
-    new_pic = ImageTk.PhotoImage(resized)
+# Welcome text in top frame
+text_label = tk.Label(top_frame, text="Welcome [name]! This is your NCEA summary...", font=("Arial", 24), bg="lightblue", fg="black")
+text_label.pack(pady=50, padx=50, side="left")
+# 3 Progress bars for NCEA credits in top frame
+l1progress = tk.Label(top_frame, text="Level 1 credits:", font=("Helvetica", 12), fg="white")
+l1progress.pack(pady=5, side="top")
+progress_bar1 = tk.Label(top_frame, relief="ridge", bg="white", width=20, height=1)
+progress_bar1.pack(pady=5, side="top")
 
-    label = tk.Label(nav_frame, image=new_pic)
-    label.image = new_pic
-    label.pack()
+l2progress = tk.Label(top_frame, text="Level 2 credits:", font=("Helvetica", 12), fg="white")
+l2progress.pack(pady=5, side="top")
+progress_bar2 = tk.Label(top_frame, relief="ridge", bg="white", width=20, height=1)
+progress_bar2.pack(pady=5, side="top")
 
-# Navigation frame
-nav_frame = tk.Frame(root, bg="white", width=200, height=400)
-nav_frame.pack(side="left", fill="y")
+l3progress = tk.Label(top_frame, text="Level 3 credits:", font=("Helvetica", 12), fg="white")
+l3progress.pack(pady=5, side="top")
+progress_bar3 = tk.Label(top_frame, relief="ridge", bg="white", width=20, height=1)
+progress_bar3.pack(pady=5, side="top")
+# Pie chart for the current NCEA level progress
+number_credits = [30, 20, 50]
+grade = ['Achieved', 'Merit', 'Excellence']
 
-overlay_image()
+fig, ax = plt.subplots(figsize=(3, 3), facecolor='lightblue')
+ax.pie(number_credits, labels=grade, autopct='%1.1f%%')
+ax.axis('equal')
 
-def navigate(page):
-# Navigation buttons
-buttons = ["Home", "Credit Summary", "Course Search", "Help Centre"]
-
-for button_text in buttons:
-    button = tk.Button(nav_frame, text=button_text, width=15, command=lambda b=button_text: navigate(b))
-    button.pack(pady=10)
+canvas = FigureCanvasTkAgg(fig, master=top_frame)
+canvas.draw()
+canvas.get_tk_widget().pack()
 
 root.mainloop()
