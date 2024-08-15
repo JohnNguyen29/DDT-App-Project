61483826ed1f1cbb900ab2cf5628b936ff2b3627
JohnNguyen29 <johnnguyen290107@gmail.com>
2024-08-15 18:22:47 +1200

    Part 1: Updated with more standards. 12ENG, 12MAT, 12PHY, 12DDT, 12DVV.

--- allstandards.py
+++ allstandards.py
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS all_standards (
     subject_code TEXT,
     description TEXT,
     assessment_type TEXT,
-    credits INTEGER
+    credits_avaliable INTEGER
 )
 ''')
 
@@ -38,12 +38,39 @@ all_standards = [
     ('13PHY', 'Physics 3.3 - Demonstrate understanding of wave systems', 'External', 4),
     ('13PHY', 'Physics 3.4 - Demonstrate understanding of mechanical systems', 'External', 6),
     ('13PHY', 'Physics 3.5 - Demonstrate understanding of Modern Physics', 'Internal', 3),
-    ('13PHY', 'Physics 3.6 - Demonstrate understanding of electrical systems', 'External', 6)
+    ('13PHY', 'Physics 3.6 - Demonstrate understanding of electrical systems', 'External', 6),
+    ('12MAT', 'Mathematics and Statistics 2.1 - Apply co-ordinate geometry methods in solving problems', 'Internal', 2),
+    ('12MAT', 'Mathematics and Statistics 2.2 - Apply graphical methods in solving problems', 'Internal', 4),
+    ('12MAT', 'Mathematics and Statistics 2.4 - Apply trigonometric relationships in solving problems', 'Internal', 3),
+    ('12MAT', 'Mathematics and Statistics 2.6 - Apply algebraic methods in solving problems', 'External', 4),
+    ('12MAT', 'Mathematics and Statistics 2.7 - Apply calculus methods in solving problems', 'External', 5),
+    ('12MAT', 'Mathematics and Statistics 2.12 - Apply probability methods in solving problems', 'External', 4),
+    ('12MAT', 'Mathematics and Statistics 2.14 - Apply systems of equations in solving problems', 'Internal', 2),
+    ('12ENG', 'English 2.1 - Analyse specified aspect(s) of studied written text(s), supported by evidence', 'External', 4),
+    ('12ENG', 'English 2.2 - Analyse specified aspect(s) of studied visual or oral text(s), supported by evidence', 'External', 4),
+    ('12ENG', 'English 2.3 - Analyse significant aspects of unfamiliar written text(s) through close reading, supported by evidence', 'External', 4),
+    ('12ENG', 'English 2.4 - Produce a selection of crafted and controlled writing', 'Internal', 6),
+    ('12ENG', 'English 2.10 - Analyse aspects of visual and/or oral text(s) through close viewing and/or listening, supported by evidence', 'Internal', 3),
+    ('12DDT', 'Digital Technologies 2.2 - Apply conventions to develop a design for a digital technologies outcome', 'Internal', 3),
+    ('12DDT', 'Digital Technologies 2.4 - Use advanced techniques to develop a digital media outcome', 'Internal', 4),
+    ('12DDT', 'Digital Technologies 2.7 - Use advanced programming techniques to develop a computer program', 'Internal', 6),
+    ('12DDT', 'Digital Technologies 2.8 - Use advanced processes to develop a digital technologies outcome', 'Internal', 6),
+    ('12DDT', 'Digital Technologies 2.10 - Present a summary of developing a digital outcome', 'External', 3),
+    ('12DVV', 'Design and Visual Communication 2.30 - Use visual communication techniques to generate design ideas', 'External', 3),
+    ('12DVV', 'Design and Visual Communication 2.33 - Use the characteristics of a design movement or era to inform own design ideas', 'Internal', 3),
+    ('12DVV', 'Design and Visual Communication 2.34 - Develop a spatial design through graphics practice', 'Internal', 6),
+    ('12DVV', 'Design and Visual Communication 2.35 - Develop a product design through graphics practice', 'Internal', 6),
+    ('12DVV', 'Design and Visual Communication 2.36 - Use visual communication techniques to compose a presentation of a design', 'Internal', 4),
+    ('12PHY', 'Physics 2.1 - Carry out a practical physics investigation that leads to a non-linear mathematical relationship', 'Internal', 4),
+    ('12PHY', 'Physics 2.3 - Demonstrate understanding of waves', 'External', 4),
+    ('12PHY', 'Physics 2.4 - Demonstrate understanding of mechanics', 'External', 6),
+    ('12PHY', 'Physics 2.5 - Demonstrate understanding of atomic and nuclear physics', 'Internal', 3),
+    ('12PHY', 'Physics 2.6 - Demonstrate understanding of electricity and electromagnetism', 'External', 6)
 ]
 
 # Insert data into the table
 cursor.executemany('''
-INSERT INTO all_standards (subject_code, description, assessment_type, credits)
+INSERT INTO all_standards (subject_code, description, assessment_type, credits_avaliable)
 VALUES (?, ?, ?, ?)
 ''', all_standards)
 
